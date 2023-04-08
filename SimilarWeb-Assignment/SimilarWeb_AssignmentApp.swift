53835840a8e54ac4baefed20ba13672a9a23bd20
Oleh Veheria <oleh@veheria.tech>
2023-04-08 13:05:26 +0300

    Initial Commit

---
+++ SimilarWeb-Assignment/SimilarWeb_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimilarWeb_AssignmentApp.swift
+//  SimilarWeb-Assignment
+//
+//  Created by Oleh Veheria on 08.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct SimilarWeb_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
